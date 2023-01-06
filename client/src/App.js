75caa342e2f7506b3f20e732acf6b45c9edab735
louvivien <vivien.richaud@gmail.com>
2023-01-07 00:49:44 +0100

    update front

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 function App() {
   const [input, setPrompt] = useState("");
   const [response, setResponse] = useState("");
   const [enrichedWithGoogleResults, setEnrichedWithGoogleResults] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // Set isLoading to true before sending the request
+      setIsLoading(true);
     // Send a request to the server with the prompt
     axios
       .post("/chat", { input, enrichedWithGoogleResults })
       .then((res) => {
         // Update the response state with the server's response
         setResponse(res.data);
+        // Set isLoading to false
+        setIsLoading(false);
+        // Reset isLoading to false after the response has been displayed
+        setIsLoading(false);
       })
       .catch((err) => {
         console.error(err);
@@ -25,68 +33,91 @@ function App() {
     
 
   return (
-    <div className="container-fluid">
-  <div className="row">
-    <div className="col-12">
-      <h1>React app ChatGPT</h1>
-      <p>This React app is connected to ChatGPT. The problem with ChatGPT is that it is normally not connected to internet, it is trained on data from 2021</p>
-    </div>
-  </div>
-  <div className="container bg-light p-3">
-  <h3>Directions</h3>
-  <p>Try the following prompts with or without Google :</p>
-  <ul>
-    <li>Who won the 2022 world cup?</li>
-    <li>Mike's mom has four kids, penny, nickel, dime, and...what is the name of the fourth kid?</li>
-    <li>Who is Vivien Richaud?</li>
-    <li>What happened to Benedict XVI?</li>
-  </ul>
-</div>
-  <div className="row mt-5">
-    <div className="col-12">
-    <Form className="form-inline" onSubmit={handleSubmit}>
-    <FormGroup switch>
-        <Input
-          type="switch"
-          checked={enrichedWithGoogleResults}
-          onChange={(e) => setEnrichedWithGoogleResults(e.target.checked)}
-              
-        />
-        <Label check>Enriched with Google results</Label>
-      </FormGroup>
-  <FormGroup>
-    <Label for="prompt">Enter your message:</Label>
-    <Input
-      type="text"
-      id="prompt"
-      value={input}
-      onChange={(e) => setPrompt(e.target.value)}
-    />
-  </FormGroup>
-  <div className="d-flex align-items-center justify-content-center">
-  <Button color="primary">Submit</Button>
-  </div>
+  <Container fluid>
+ <Row>
+        <Col xs={12}>
+          <h1>React app ChatGPT</h1>
+          <p>This React app is connected to ChatGPT. The problem with ChatGPT is that it is normally not connected to internet, it is trained on data from 2021</p>
+        </Col>
+      </Row>
+      <Row>
 
-</Form>
-<div >
-  {response ? (
-    <>
-    <div style={{ marginTop: "20px" }}>
-      <div className="container-fluid">
-          <div className="row">
-            <div style={{ display: "flex", alignItems: "flex-start" }}>
-              <img src="/images/logochatgpt.jpeg" alt="ChatGPT logo" />
-              <div style={{ marginLeft: "10px", }}>{response}</div>
+        <Col xs={12} className="bg-light p-3">
+          <h3>Directions</h3>
+          <p>Try the following prompts with or without Google :</p>
+          <ul>
+            <li>Who won the 2022 world cup?</li>
+            <li>Mike's mom has four kids, penny, nickel, dime, and...what is the name of the fourth kid?</li>
+            <li>Who is Vivien Richaud?</li>
+            <li>What happened to Benedict XVI?</li>
+          </ul>
+        </Col>
+
+      </Row>
+
+      <Row className="mt-5">
+        <Col xs={12}>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup check>
+              <Label check>
+                <Input
+                  type="checkbox"
+                  checked={enrichedWithGoogleResults}
+                  onChange={(e) => setEnrichedWithGoogleResults(e.target.checked)}
+                />{' '}
+                Enriched with Google results
+              </Label>
+            </FormGroup>
+            <FormGroup>
+              <Label for="prompt">Enter your message:</Label>
+              <Input
+                type="text"
+                id="prompt"
+                value={input}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+            </FormGroup>
+            <div className="text-center">
+                <Button color="primary" type="submit">Submit</Button>
             </div>
-          </div>
-      </div>
-      </div>
-    </>
+          </Form>
+        </Col>
+      </Row>
+
+
+{isLoading ? (
+
+<div style={{ marginTop: "10px", }}>
+
+  <Container>
+      <Row>
+        <Col className="text-center">Loading...</Col>
+      </Row>
+  </Container>
+
+</div>
+
+) : (
+
+<div>
+  {response ? (
+    <Container style={{ marginTop: '20px' }}>
+      <Row>
+        <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+          <img src="/images/logochatgpt.jpeg" alt="ChatGPT logo" />
+          <div style={{ marginLeft: '10px' }}>{response}</div>
+        </div>
+      </Row>
+    </Container>
   ) : null}
 </div>
-    </div>
-  </div>
-</div>
+
+
+)}
+
+</Container>
+
+
 
   );
 }
---
+++ client/src/Thinking.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Thinking = () => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <div className="container-fluid">
+          <div className="row">
+            <div style={{ display: "flex", alignItems: "flex-start" }}>
+              <img src="/images/logochatgpt.jpeg" alt="ChatGPT logo" />
+              <div style={{ marginLeft: "10px", }}>Loading...</div>
+            </div>
+          </div>
+      </div>
+      </div>
+  )
+}
+
+export default Thinking
